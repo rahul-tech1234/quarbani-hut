0356755a9d395f40356333395c19f3195c4e1bb1
rahul-tech1234 <rahul.laravel.dev@gmail.com>
2026-06-15 17:00:23 +0600

    new deploy

--- src/apicall.jsx
+++ src/apicall.jsx
@@ -1,5 +1,5 @@
 export const data=async()=>{
-    const res=await fetch('https://quarbani-hut-sigma.vercel.app/data.json');
+    const res=await fetch('https://qurbani-hut-snowy.vercel.app/data.json');
     const data=await res.json();
     return data;
 };
